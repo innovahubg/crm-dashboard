74a54c5be63c2272cb25d7b61587cdbfd312c566
Harland Lohora <harland@lohora.com>
2024-03-01 00:16:12 -0600

    :construction: automation calendar

---
+++ src/Pages/Automation/CalendarComponent.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction"
+
+
+
+
+const CalendarComponent = ({ setModal, setType, setDetails }) => {
+
+
+    const handleDateClick = (arg) => {
+        console.log(arg.dateStr)
+        setType("new")
+        setModal(true)
+
+    }
+
+    const handleEventClick = (info) => {
+        info.jsEvent.preventDefault(); // don't let the browser navigate
+        console.log(info.event.title, info.event.id)
+        setDetails({ title: info.event.title })
+        setType("details")
+        setModal(true)
+    }
+    return (
+        <div>
+            <FullCalendar
+                plugins={[dayGridPlugin, interactionPlugin]}
+                initialView="dayGridMonth"
+                events={[
+                    { title: 'event 1', date: '2024-03-01', id: 1996 },
+                    { title: 'event 2', date: '2024-03-01', id: 2123 }
+                ]}
+                eventClick={handleEventClick}
+                dateClick={handleDateClick}
+            />
+        </div>
+    )
+}
+
+export default CalendarComponent
--- src/Pages/Automation/index.js
+++ src/Pages/Automation/index.js
@@ -1,18 +1,102 @@
-import React from "react";
-
-import { Container } from "reactstrap";
-
-//Import Breadcrumb
+import React, { useState } from "react";
 import Breadcrumbs from "../../components/Common/Breadcrumb";
+import CalendarComponent from "./CalendarComponent";
+
+import {
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Input,
+  UncontrolledDropdown,
+  Container,
+  Row,
+  Col,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Button,
+} from "reactstrap";
 
 const Automation = () => {
+
+  const [modal, setModal] = useState(false)
+  const [type, setType] = useState("")
+  const [details, setDetails] = useState({})
   return (
     <React.Fragment>
       <div className="page-content">
         <Container fluid={true}>
           <Breadcrumbs title="Inicio" breadcrumbItem="Automatización" />
         </Container>
-        Automatización
+        <CalendarComponent setModal={setModal} setType={setType} setDetails={setDetails} />
+        <Modal
+          isOpen={modal}
+          toggle={() => {
+            setModal(false);
+          }}
+          centered
+        >
+          <ModalHeader
+            className="bg-light p-3"
+            id="exampleModalLabel"
+            toggle={() => setModal(false)}
+          >
+            {type === "new" && <>Nuevo evento</>}
+            {type === "details" && <>Detalles</>}
+
+          </ModalHeader>
+          <form className="tablelist-form">
+            <ModalBody style={{ height: "auto" }}>
+              {
+                type === "new" && (<div className="mb-3">
+                  <label htmlFor="titlebot-field" className="form-label">
+                    Nombre
+                  </label>
+                  <input
+                    type="text"
+                    id="titlebot-field"
+                    className="form-control"
+                    placeholder="Ingresa un nombre a tu evento"
+                    required
+                    value={""}
+                    onChange={(e) =>
+                      console.log({})
+                    }
+                  />
+                </div>)
+              }
+
+              {
+                type === "details" && (<div className="mb-3">
+                  <h4 htmlFor="titlebot-field" className="form-label">
+                    Titulo: {details.title}
+                  </h4>
+
+                </div>)
+              }
+
+            </ModalBody>
+            <ModalFooter>
+              <div className="">
+                <button
+                  type="button"
+                  className="btn btn-light mx-4"
+                  onClick={() => setModal(false)}
+                >
+                  Cerrar
+                </button>
+                <button
+                  onClick={() => { }}
+                  className="btn btn-success"
+                  id="add-btn"
+                >
+                  Crear
+                </button>
+              </div>
+            </ModalFooter>
+          </form>
+        </Modal>
       </div>
     </React.Fragment>
   );
