f21fd438c58a4a9bbb2fca8fd85a47e61d7763ef
Harland Lohora <harland@lohora.com>
2024-03-03 22:28:58 -0600

    tabs auto

--- src/Pages/Automation/index.js
+++ src/Pages/Automation/index.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Breadcrumbs from "../../components/Common/Breadcrumb";
 import CalendarComponent from "./CalendarComponent";
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+import { GetData } from "../../services/api";
 import {
   DropdownItem,
   DropdownMenu,
@@ -17,19 +19,126 @@ import {
   ModalHeader,
   Button,
 } from "reactstrap";
+import { Link } from "react-router-dom";
+import { RingLoader } from "react-spinners";
+import DataTable from "react-data-table-component";
+
 
 const Automation = () => {
 
   const [modal, setModal] = useState(false)
   const [type, setType] = useState("")
   const [details, setDetails] = useState({})
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await GetData("/contact-lists");
+      setData(data);
+      console.log({ data });
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  const columns = [
+    {
+      name: <span className="font-weight-bold fs-13">Nombre</span>,
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: <span className="font-weight-bold fs-13">Compañia</span>,
+      selector: (row) => row.companyId,
+      sortable: true,
+    },
+    {
+      name: <span className="font-weight-bold fs-13">Acción</span>,
+      sortable: true,
+
+      cell: (data) => {
+        return (
+          <UncontrolledDropdown className="dropdown d-inline-block">
+            <DropdownToggle
+              className="btn btn-soft-secondary btn-sm"
+              tag="button"
+            >
+              <i className="ri-more-fill align-middle"></i>
+            </DropdownToggle>
+            <DropdownMenu className="dropdown-menu-end">
+              <DropdownItem>
+                <Link to={`/campaigns/${data.id}`} className="text-muted">
+                  <i className="ri-eye-fill align-bottom me-2 text-muted"></i>
+                  Detalle {data.name}
+                </Link>
+              </DropdownItem>
+              <DropdownItem className="edit-item-btn">
+                <i className="ri-pencil-fill align-bottom me-2 text-muted"></i>
+                Edit
+              </DropdownItem>
+              <DropdownItem className="remove-item-btn">
+                {" "}
+                <i className="ri-delete-bin-fill align-bottom me-2 text-muted"></i>{" "}
+                Delete{" "}
+              </DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        );
+      },
+    },
+  ];
+
   return (
     <React.Fragment>
       <div className="page-content">
         <Container fluid={true}>
           <Breadcrumbs title="Inicio" breadcrumbItem="Automatización" />
         </Container>
-        <CalendarComponent setModal={setModal} setType={setType} setDetails={setDetails} />
+
+        <Tabs>
+          <TabList>
+            <Tab>Registro</Tab>
+            <Tab>Agendados</Tab>
+            <Tab>Calendario</Tab>
+          </TabList>
+
+          <TabPanel>
+            <Row className="mb-4">
+              {loading ? (
+                <div className="d-flex justify-content-center p-5 w-full">
+                  <RingLoader color="#E9553E" />
+                </div>
+              ) : (
+                <DataTable
+                  data={data}
+                  columns={columns}
+                  noDataComponent={<span className="py-4">Sin resultados</span>}
+                />
+              )}
+            </Row>
+          </TabPanel>
+          <TabPanel>
+            <Row className="mb-4">
+              {loading ? (
+                <div className="d-flex justify-content-center p-5 w-full">
+                  <RingLoader color="#E9553E" />
+                </div>
+              ) : (
+                <DataTable
+                  data={data}
+                  columns={columns}
+                  noDataComponent={<span className="py-4">Sin resultados</span>}
+                />
+              )}
+            </Row>
+          </TabPanel>
+          <TabPanel>
+            <CalendarComponent setModal={setModal} setType={setType} setDetails={setDetails} />
+          </TabPanel>
+        </Tabs>
+
+
         <Modal
           isOpen={modal}
           toggle={() => {
